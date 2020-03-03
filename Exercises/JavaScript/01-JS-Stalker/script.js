bbaafe18239df1153227908ff05376311b75ddde
chenMac <jinchenhu.luke@gmail.com>
2020-03-02 21:19:26 -0500

    upload js exs

---
+++ Exercises/JavaScript/01-JS-Stalker/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What's your last name");
+var age = prompt("How old are you?");
+
+console.log("You full name is " + firstName + " " + lastName);
+console.log("Your are " + age + " years old");
